937abce77d4c2ba923234945e74841fdb5cb94ab
nereadelbarrio <49400164+nereadelbarrio@users.noreply.github.com>
2020-02-06 12:15:06 +0100

    Update style.js

--- style.js
+++ style.js
@@ -27,7 +27,7 @@
 				{ name: 'NumeroIntro', element: 'span', attributes: { 'class': 'bck-enfasis-1'} },
 				{ name: 'FontInfantil', element: 'span', attributes: { 'class': 'bck-enfasis-2'} },
 				{ name: 'FontInfantilPuntos', element: 'span', attributes: { 'class': 'bck-enfasis-3'} },
-				{ name: 'FontEjemploCursiva', element: 'span', attributes: { 'class': 'bck-enfasis-4'} },
+				{ name: 'FontEjemplos', element: 'span', attributes: { 'class': 'bck-enfasis-4'} },
 				{ name: 'FontHurmeGeometric', element: 'span', attributes: { 'class': 'bck-enfasis-5'} },
 				
 				{ name: 'Lista Desordenada', element: 'ul', attributes: { 'class': 'bck-ul-3'} },
