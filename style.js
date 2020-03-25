3bc590c23d926da5d2fded9aa884c28cb2ba4337
nereadelbarrio <49400164+nereadelbarrio@users.noreply.github.com>
2020-03-25 13:34:16 +0100

    Update style.js

--- style.js
+++ style.js
@@ -84,6 +84,7 @@
 				{ name: 'Caja 28', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-28' } },
 				{ name: 'Caja 29', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-29' } },
 				{ name: 'Caja 30', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-30' } },
+				{ name: 'Caja 31', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-31' } },
 
 				{ name: 'Tabla Color', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table-2'} },
 				{ name: 'Tabla 3', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table-3'} },
